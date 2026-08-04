fc02abfeb1b1bbdef6df52b9c3a8e1cccecbc63f
Omkar Sutar <omkarsutar37@gmail.com>
2026-08-04 22:04:44 +0530

    brand id added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1052681739"
+    serviceWorkerVersion: "3505227943"
   }
 });
